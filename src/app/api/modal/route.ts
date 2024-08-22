a7b6d17958f2f3bca9b6c06f2ec7263f3a1fcd77
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2024-08-22 18:57:39 +0200

    i

--- src/app/api/modal/route.ts
+++ src/app/api/modal/route.ts
@@ -2,7 +2,7 @@ import { headers } from 'next/headers'
  
 export async function GET(request: Request) {
   const headersList = headers()
-  const referer = headersList.get('referer')
+  const referer = headersList.get('referer') ?? ''
  
   return new Response('Hello, Next.js!', {
     status: 200,
