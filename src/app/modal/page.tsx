9d1b51fc4518f4ee5e996889de0f5fcf564a9699
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2024-08-22 19:32:32 +0200

    i

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -1,15 +1,20 @@
  "use client"
 
+import ClientDetails from "@/components/client-details";
 import AppExtensionsSDK from "@pipedrive/app-extensions-sdk"
 import { useEffect } from "react"
-
 
 
 
 export default function Page() {
     useEffect(()=> {
         if (window === undefined) return;
-        new AppExtensionsSDK().initialize()
+        // new AppExtensionsSDK().initialize()
     })
-    return <h1>MODAL!</h1>
+
+    return (
+        <div className="form-container">
+            <ClientDetails/>
+        </div>
+    );
 }
---
+++ src/components/client-details.tsx
@@ -0,0 +1,11 @@
+export default function ClientDetails() {
+    return (
+        <div className="form-section">
+        <h3>Client details</h3>
+        <input type="text" placeholder="First name" />
+        <input type="text" placeholder="Last name" />
+        <input type="text" placeholder="Phone" />
+        <input type="email" placeholder="Email (optional)" />
+      </div>
+    )
+}
