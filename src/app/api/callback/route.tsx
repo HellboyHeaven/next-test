8f587c8f2954bffbedd308a7f7df4d1823a40fa3
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2024-08-23 22:53:25 +0200

    fixed for sure sure

--- src/app/api/callback/route.tsx
+++ src/app/api/callback/route.tsx
@@ -3,21 +3,21 @@ import  {getLoggedInUser, initalizeSession, initAPIClient, updateTokens} from ".
 import { cookies } from "next/headers.js";
 
 async function GET(request : NextRequest) {
-    // const params = request.nextUrl.searchParams;
-    // try {
-    //     const code = params.get('code') as string;
-    //     // Get the access token
-    //     const apiClient = initAPIClient();
-    //     const token = await apiClient.authorize(code);
+    const params = request.nextUrl.searchParams;
+    try {
+        const code = params.get('code') as string;
+        // Get the access token
+        const apiClient = initAPIClient();
+        const token = await apiClient.authorize(code);
 
-    //     updateTokens(apiClient, token);
-    //     // Get the currently logged in user
-    //     const user = await getLoggedInUser(apiClient);
-    //     const me = user.data;
-    //     //initalize session
-    //     initalizeSession(cookies, token, me.id)
-    //     return new Response('Successfully authorized', {status: 200})
-    // } catch (error) {
-    //     return new Response( 'error' , {status: 200})
-    // }
+        updateTokens(apiClient, token);
+        // Get the currently logged in user
+        const user = await getLoggedInUser(apiClient);
+        const me = user.data;
+        //initalize session
+        initalizeSession(cookies, token, me.id)
+        return new Response('Successfully authorized', {status: 200})
+    } catch (error) {
+        return new Response( 'error' , {status: 200})
+    }
 };
