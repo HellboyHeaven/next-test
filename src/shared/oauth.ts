d6900840b60b560141b719c6c4a1f42f0f47d4ed
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2024-08-25 16:07:58 +0200

    custom pipedrive api added 2

--- src/shared/oauth.ts
+++ src/shared/oauth.ts
@@ -1,5 +1,5 @@
 import { getCookie, setCookie } from 'cookies-next';
-import { ApiClient, DealsAPI } from './pipedriveAPI';
+import { ApiClient } from './pipedriveAPI';
 import { CookiesFn } from 'cookies-next/lib/types';
 import { cookies } from 'next/headers';
 
@@ -42,63 +42,6 @@ export const getAPIClient = (cookies : CookiesFn) : ApiClient => {
   const session = getCookie('session', { cookies }) as string;
   return JSON.parse(session).apiClient as ApiClient
 }
-  
-//   // Generate the authorization URL for the 1st step
-// export const getAuthorizationUrl = (apiClient) => {
-//   const authUrl = apiClient.buildAuthorizationUrl();
-//   console.log('Authorization URL generated');
-//   return authUrl;
-// };
-  
-// // Get the currently authorized user details
-// export const getLoggedInUser = async (apiClient) => {
-//   const api = new UsersApi(apiClient);
-//   const data = await api.getCurrentUser();
-//   console.log('Currently logged-in user details obtained');
-//   return data;
-// };
-  
-// // Update Access and Refresh tokens
-// export const updateTokens = (apiClient, token) => {
-//   console.log('Updating access + refresh token details');
-//   const oauth2 = client.authentications.oauth2;
-//   oauth2.accessToken = token.access_token;
-//   oauth2.refreshToken = token.refresh_token;
-// };
-
-
-// export const initalizeSession = async ( cookies, accessToken, userId) => {
-//   try {
-//     // 1.1 Check if the session cookie is already set
-//     console.log(`Checking if a session cookie is set for ${userId}`);
-//     const session = getCookie('session', { cookies }); 
-
-//     if (!session) {
-//       log.info('Session cookie no found. Session cookie set 🍪');
-//       return setSessionCookie(
-//         true,
-//         userId,
-//         accessToken,
-//         String(Date.now() + 59 * 60 * 1000),
-//         req,
-//         res
-//       );
-//     }
-    
-//     console.log('Session cookie found 🍪');
-//     return JSON.parse(session);
-    
-    
-
-    
-//   }
-//   catch {
-//     console.error("Couldn't create session :[");
-//     console.error(error);
-//   }
-  
-// }
-
 
 const setSessionCookie = (apiClient: ApiClient, cookies : CookiesFn) => {
   const newSession = {
