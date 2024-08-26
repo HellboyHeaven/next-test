9185522f0cee4294da24fc45a2ff20fe5178760a
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2024-08-26 18:24:48 +0200

    add debug 7

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,13 +7,14 @@ import { pipedriveAuth } from './shared/pipedriveAuth'
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   try {
-    const apiClient = JSON.parse(cookies().get('apiClient')?.value as string) as ApiClient
+    const cookie = cookies().get('apiClient')?.value as string
+    if (cookie === undefined) throw Error('no api client cookie')
+    const apiClient = JSON.parse(cookie) as ApiClient
     if (apiClient === undefined || apiClient.token === undefined) {
       throw Error('no token')
     }
     console.log('expires in: ', (Date.now() - apiClient.token.expireAt)/1000)
     if (apiClient.token.expireAt < Date.now()) {
-      
       await apiClient.updateToken()
     }
   } catch(error : Error | any) {
