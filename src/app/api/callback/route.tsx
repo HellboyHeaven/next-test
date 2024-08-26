90248b7e31865cfd4d0f3d35b9c9a9428c6882fa
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2024-08-26 20:28:07 +0200

    add debug 14

--- src/app/api/callback/route.tsx
+++ src/app/api/callback/route.tsx
@@ -6,16 +6,13 @@ import { cookies } from "next/headers.js";
 export async function GET(request : NextRequest) {
     const params = request.nextUrl.searchParams;
     const code = params.get('code') as string;
-    const state = params.get('state') as string;
+
     if (!code) return new Response( 'error', {status:401})
     try {
 
         // Get the access token
         const client = await initAPIClient(code);
         cookies().set('apiClient', JSON.stringify(client), {sameSite: 'none', secure: true});
-        if (state !== undefined ) {
-            return NextResponse.redirect(state)
-        }
 
         return new Response('Successfully authorized', {status: 200})
     } catch (error : any) {
