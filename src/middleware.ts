5bcf04ada1126e5cd8d169bba5c96f6ed7789300
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2024-08-26 18:30:47 +0200

    add debug 9

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,9 +21,9 @@ export async function middleware(request: NextRequest) {
     const client = pipedriveAuth();
     console.log('pipeAuth',  JSON.stringify(client), '\n', request.redirect.toString())
     console.log(error.message)
-    // const authURL = client.buildAuthURL(request.nextUrl.toString())
-   
-    // return NextResponse.redirect(authURL)
+    const authURL = client.buildAuthURL(request.nextUrl.toString())
+    console.log(authURL)
+    return NextResponse.redirect(authURL)
   }
  
 }
