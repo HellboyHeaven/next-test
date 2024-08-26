b75e9097c8f2eb4a88c6a6181824ec0a791b75ec
HellboyHeavens <49723005+HellboyHeaven@users.noreply.github.com>
2024-08-26 20:40:53 +0200

    debug 2

--- src/app/modal/createDeal.ts
+++ src/app/modal/createDeal.ts
@@ -7,12 +7,12 @@ import { cookies } from "next/headers";
 
 export async function createDeal(dataForm : FormData) {
     const cookie = cookies().get('apiClient')?.value as string
-    console.log(cookie)
+   
     try {
         const apiClient = JSON.parse(cookie) as ApiClient
         const dealsApi = new DealsAPI(apiClient);
-        const res = await dealsApi.addDeal('job', {custom_fields: dataForm})
-        console.log(res)
+        // const res = await dealsApi.addDeal('job', {custom_fields: dataForm})
+        console.log(JSON.stringify(apiClient))
     } catch (error : any) {
         console.log(error.message)
     }
